035413ccedbb531e025ea313dded1b36e136c8f1
Brian <brian.sila1@student.moringaschool.com>
2025-04-12 21:31:41 +0300

    final commit

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,21 @@
 import React from "react";
 
-function Article({ title, date = "January 1, 1970", preview }) {
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+    // bonus challenge
+    const renderReadingTime = (minutes) => {
+        if (minutes < 30){
+            const coffeeCups = Math.ceil(minutes / 5);
+            return `${'☕'.repeat(coffeeCups)} ${minutes} min read`;
+        } else {
+            const bentoBoxes = Math.ceil(minutes / 10);
+            return `${'🍱'.repeat(bentoBoxes)} ${minutes} min read`;
+        }
+    }
+
   return (
     <article>
       <h3>{title}</h3>
-      <small>{date}</small>
+      <small>{date} . {renderReadingTime(minutes)}</small>
       <p>{preview}</p>
     </article>
   );
